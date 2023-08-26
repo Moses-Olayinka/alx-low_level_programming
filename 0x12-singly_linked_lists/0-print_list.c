47b5678bac59f6dafbb44ae237af2caa7f43b32e
moses-olayinka <mosesolayinkaa@gmail.com>
2023-08-25 18:29:34 -0700

    Prints Elements

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include <stdio>
+#include "lists.h"
+
+/**
+ * print_list - print all the elements of structure list
+ * @h: the structure pointer
+ *
+ * Return: the number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t counter = 0;
+
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+
+		counter++;
+		h = h->next;
+	}
+
+	return (counter);
+}
