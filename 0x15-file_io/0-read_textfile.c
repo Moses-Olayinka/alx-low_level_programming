f48f6b24b34ceea42e872eb8d5fe3cc100d3fe52
Moses-Olayinka <mosesolayinkaa@gmail.com>
2023-10-03 11:52:39 -0700

    Updated

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,34 +13,24 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t rdfil, wrfil;
 	char *buff;
 
-	if (filename == NULL)
+	if (!filename)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
+
 	if (fd == -1)
 		return (0);
 
-	buff = malloc(sizeof(char) * letters);
-	if (buff == NULL)
+	buff = malloc(sizeof(char) * (letters));
+	if (!buff)
 		return (0);
 
 	rdfil = read(fd, buff, letters);
-	if (rdfil == -1)
-	{
-		free(buff);
-		close(fd);
-		return (0);
-	}
-
 	wrfil = write(STDOUT_FILENO, buff, rdfil);
-	if (wrfil == -1)
-	{
-		free(buff);
-		close(fd);
-		return (0);
-	}
 
 	close(fd);
 
-	return (rdfil);
+	free(buff);
+
+	return (wrfil);
 }
